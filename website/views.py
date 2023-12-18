da2d2b9b29f36ad5d684242aaa7da15a5cecee4d
Christian Jasper Buenaventura <147928766+Ginx55@users.noreply.github.com>
2023-12-18 16:27:18 +0800

    update

--- website/views.py
+++ website/views.py
@@ -1893,6 +1893,7 @@ def SaveUser(request):
                     imgsrc = getImageURL("user_profiles/", uniqueID)
 
                     changed_fields['Profile Picture'] = {
+                        "old": "",
                         "new": "new profile picture",
                     }
                 else:
